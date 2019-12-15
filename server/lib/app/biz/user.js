1bdba87162046751915caf59ac3dcdadc312068d
kylin <chang.qinglin@xinheyun.com>
2019-12-15 17:27:39 +0800

    server dev

--- server/lib/app/biz/user.js
+++ server/lib/app/biz/user.js
@@ -1,9 +1,24 @@
 const _ = require('lodash')
 const fs = require('fs')
 const moment = require('moment')
-
+const redis = require('../../runtime/redis')
 const User = require('../../models/user'),
+  Reward = require('../../models/reward'),
   validateLogin = require('../validators/validateLogin')
+
+const getRandomIndex = (max) => {
+  return Math.floor(Math.random() * max) + 1
+}
+
+const rewardMap = {
+  1: '谢谢参与',
+  2: '问答卡1',
+  3: '问答卡2',
+  4: '问答卡3',
+  5: '吉祥物主场球衣',
+  6: '吉祥物客场球衣',
+  7: '礼包奖品',
+}
 
 // 用户登录
 module.exports.get = {
@@ -42,7 +57,7 @@ module.exports.register = {
         telephone
       }).exec((err, user) => {
         if (user) {
-           // todo 计算用户次数
+          // todo 计算用户次数
           res.$locals.writeData({ user })
           req.$session.setUser(user)
           next()
@@ -53,7 +68,7 @@ module.exports.register = {
           newUser.times = 1
           newUser.refreshDate = new Date
           newUser.save(err => {
-            res.$locals.writeData({ user:newUser })
+            res.$locals.writeData({ user: newUser })
             req.$session.setUser(newUser)
             next()
           })
@@ -63,13 +78,7 @@ module.exports.register = {
   ]
 }
 
-// 1 谢谢参与
-// 2 问答卡1
-// 3 问答卡2
-// 4 问答卡3
-// 5 吉祥物主场球衣
-// 6 吉祥物客场球衣
-// 7 礼包奖品
+
 
 // 抽奖接口：
 module.exports.lottery = {
@@ -79,25 +88,55 @@ module.exports.lottery = {
     // 1 判断抽奖次数
     (req, res, next) => {
       let curUser = req.$injection.user
-      if(curUser.times>0){
+      if (curUser.times > 0) {
         next()
-      }else{
+      } else {
         next(new Error('您的抽奖次数已经用完'))
       }
     },
-     // 2 执行抽奖
-    (req, res, next) => {
+    // 2 执行抽奖
+    async (req, res, next) => {
+      let curUser = req.$injection.user
+      let randomIndex
       // 先判断用户是否有过实物奖励
-
-      // 在判断奖品是否还有
-      // [5,6,7]
-      // 生成一个1-4的随机数
-      console.log(2)
+      if (!curUser.hasReward) {
+        const client = redis.client
+        // const redisKey= moment().format('YYYY-MM-DD')
+        const redisKey = '2019-12-20'
+        let rewardData = await client.getAsync(redisKey)
+        rewardData = JSON.parse(rewardData)
+        // 如果还有奖品 并且抽中了
+        if (rewardData['5'] || rewardData['6'] || rewardData['7']) {
+          const tempRandomIndex = getRandomIndex(7)
+          // 抽中奖品
+          if (rewardData[tempRandomIndex]) {
+            randomIndex = tempRandomIndex
+            // 更新redis
+            client.set(redisKey, JSON.stringify({
+              ...rewardData,
+              [randomIndex]: 0
+            }))
+          }
+        }
+      }
+      randomIndex = randomIndex || getRandomIndex(4)
+      req.randomIndex = randomIndex
       next()
     },
-      // 3 减去次数 & 记录抽奖
-    (req, res, next) => {
-      console.log(3)
+    // 3 减去次数 & 记录抽奖
+    async (req, res, next) => {
+      const randomIndex = req.randomIndex
+      let user = req.$injection.user
+      let reward = new Reward()
+      reward.type = randomIndex
+      reward.userRef = user._id
+      await reward.save()
+      // 减去次数
+      await User.findByIdAndUpdate(user._id, { times: user.times - 1 })
+      res.$locals.writeData({
+        result: randomIndex,
+        recordId: reward._id
+      })
       next()
     },
   ]
@@ -105,7 +144,134 @@ module.exports.lottery = {
 
 // 抽奖历史接口：
 // get /api/user/history
+module.exports.history = {
+  method: 'get',
+  middlewares: [
+    validateLogin,
+    // 1 判断抽奖次数
+    async (req, res, next) => {
+      let curUser = req.$injection.user
+      let list = await Reward.find({ userRef: curUser._id, type: { $gt: 1 } })
+      res.$locals.writeData({ data: list.map(item=>({
+        name:rewardMap[item.type]
+      })) })
+      next()
+    },
+  ]
+}
 
+
+// 初始化抽奖数据 正式上线后 要删除此代码
+module.exports.reset = {
+  method: 'get',
+  middlewares: [
+    async (req, res, next) => {
+      let client = redis.client
+      let rewards = [
+        {
+          key: '2019-12-20',
+          data: {
+            5: 1,
+            6: 1,
+            7: 1
+          }
+        }, {
+          key: '2019-12-21',
+          data: {
+            5: 1,
+            6: 0,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-22',
+          data: {
+            5: 1,
+            6: 0,
+            7: 1
+          }
+        },
+        {
+          key: '2019-12-23',
+          data: {
+            5: 1,
+            6: 0,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-24',
+          data: {
+            5: 0,
+            6: 1,
+            7: 1
+          }
+        },
+        {
+          key: '2019-12-25',
+          data: {
+            5: 1,
+            6: 0,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-26',
+          data: {
+            5: 0,
+            6: 1,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-27',
+          data: {
+            5: 0,
+            6: 0,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-28',
+          data: {
+            5: 0,
+            6: 1,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-29',
+          data: {
+            5: 0,
+            6: 0,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-30',
+          data: {
+            5: 0,
+            6: 1,
+            7: 0
+          }
+        },
+        {
+          key: '2019-12-31',
+          data: {
+            5: 0,
+            6: 0,
+            7: 1
+          }
+        }
+      ]
+
+      rewards.forEach(item => {
+        client.set(item.key, JSON.stringify(item.data))
+      })
+      next()
+    },
+  ]
+}
 // 无用
 module.exports.login = {
   method: 'post',
--- server/lib/models/reward.js
+++ server/lib/models/reward.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose'),
 
 const schema = Schema({
   userRef:{ type: Schema.Types.ObjectId, ref: 'user' },
-  reward:String,
+  type:Number,
   name: String, // 姓名
   telephone: Number, //手机号
-  address: Date, // 上次刷新日期，每次登录/用户注册的时候判断，如果refreshDate不是今天，那么改成今天 & times设置成1
+  address: String, // 上次刷新日期，每次登录/用户注册的时候判断，如果refreshDate不是今天，那么改成今天 & times设置成1
 }, {
   timestamps: true
 });
